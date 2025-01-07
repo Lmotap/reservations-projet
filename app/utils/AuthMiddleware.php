7f1c4fc7676a1892d57dab6d8defbf89cadb12c8
Lucas Mota <mota.lucas@proton.me>
2025-01-07 12:25:38 +0100

    feat ( config ) : add Render, change multiple route for admin and user

--- app/utils/AuthMiddleware.php
+++ app/utils/AuthMiddleware.php
@@ -1,18 +1,21 @@
 <?php
 
-class AuthMiddleware {
-    public static function isAuthenticated() {
+class AuthMiddleware
+{
+    public static function isAuthenticated()
+    {
         if (!isset($_SESSION['user'])) {
             header('Location: /login');
-            exit;
+            exit();
         }
     }
 
-    public static function isAdmin() {
+    public static function isAdmin()
+    {
         self::isAuthenticated();
         if ($_SESSION['user']['role'] !== 'admin') {
             header('Location: /activities');
-            exit;
+            exit();
         }
     }
 }
